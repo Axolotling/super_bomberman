dcf9718d0f66ec0b7934243d2076119288088ee1
Bartosz Rychcik <wyimaginowany1997@gmail.com>
2018-03-17 22:58:47 +0100

    Dodano klase planszy

--- super_bomberman/BombermanGame.cpp
+++ super_bomberman/BombermanGame.cpp
@@ -2,6 +2,7 @@
 #include "Player.h"
 #include "Steering.h"
 #include "KeyboardSteering.h"
+#include "Board.h"
 #include <iostream>
 #include <SFML/System/Clock.hpp>
 
@@ -9,21 +10,13 @@ Player* BombermanGame::add_player(int x, int y)
 {
 	Player* new_player = new Player(this, x, y);
 	players.push_front(new_player);
-	board[x][y] = new_player;
+	this->board->set_object({x,y},new_player); //dodajemy obiekt gracza na pozycje x,y (BR)
 	return new_player;
 }
 
-BombermanGame::BombermanGame(const int& board_width, const int& board_height): board_width(board_width), board_height(board_height)
+BombermanGame::BombermanGame()
 {
-	board = new BoardObject**[board_width];
-	for (int i = 0; i < board_width; i++)
-	{
-		board[i] = new BoardObject*[board_height];
-		for (int j = 0; j < board_height; j++)
-		{
-			board[i][j] = nullptr;
-		}
-	}
+	this->board = new Board();
 	local_player = add_player(0, 0);
 }
 
@@ -75,14 +68,14 @@ void BombermanGame::loop()
 		if (clock.getElapsedTime().asMilliseconds() >= time_of_last_tick + milliseconds_between_ticks)
 		{
 			tick = true;
-			time_of_last_tick = time_of_last_tick + milliseconds_between_ticks;
+			time_of_last_tick = time_of_last_tick + int(milliseconds_between_ticks);
 		}
 
 		
 
 		if (tick)
 		{
-			if (local_player->board_x < board_width - 1)
+			if (local_player->board_x < board->get_map_size().first-1)
 			{
 				//local_player->move_player(1, 0);
 				bool pressed_up_or_down = false;
--- super_bomberman/BombermanGame.h
+++ super_bomberman/BombermanGame.h
@@ -4,7 +4,7 @@
 
 
 class Player;
-class BoardObject;
+class Board;
 
 class BombermanGame
 {
@@ -19,16 +19,14 @@ private:
 // Public fields
 public:
 	// plansza - zawiera informacje o tym co znajduje siê w ka¿dym miejscu planszy
-	BoardObject ***board; //board[x][y]->metoda() pozwala wywo³aæ metodê obiektu
-	// wymiary siatki planszy gry
-	const int board_width, board_height;
+	Board* board;
 	std::list<Player*> players;
 	Player* local_player;
 
 	int publiczne_cos = 2;
 
 // Constructors and destructors
-	BombermanGame(const int& board_width, const int& board_height);;
+	BombermanGame();
 
 	~BombermanGame() = default;
 
