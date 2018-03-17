f46381974e5896007d238eeb807a5ee5ed17e320
Jonasz Rudnik <rudnik.jonasz@gmail.com>
2018-03-17 19:06:12 +0100

    stworzono klasy skrzynek i kamieni

---
+++ super_bomberman/Crate.cpp
@@ -0,0 +1,13 @@
+#include "Crate.h"
+
+
+
+Crate::Crate(BombermanGame *bomberman_game, const double &board_x, const double &board_y) : BoardObject(bomberman_game, board_x, board_y, false, true)
+{
+
+}
+
+
+Crate::~Crate()
+{
+}
